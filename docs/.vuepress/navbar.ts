583cbf21353eb58d41505d66604c70face0da505
Zhangbokai614 <64344118+Zhangbokai614@users.noreply.github.com>
2023-12-20 15:47:00 +0800

    Update: upload MySQL article

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,51 +2,39 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/demo/",
   {
-    text: "博文",
+    text: "MySQL",
     icon: "pen-to-square",
-    prefix: "/posts/",
+    prefix: "/mysql/",
     children: [
       {
-        text: "苹果",
+        text: "InnoDB",
         icon: "pen-to-square",
-        prefix: "apple/",
+        prefix: "innodb/",
         children: [
-          { text: "苹果1", icon: "pen-to-square", link: "1" },
-          { text: "苹果2", icon: "pen-to-square", link: "2" },
-          "3",
-          "4",
+          "buffer_pool",
         ],
       },
       {
-        text: "香蕉",
+        text: "Data Type",
         icon: "pen-to-square",
-        prefix: "banana/",
+        prefix: "date_type/",
         children: [
-          {
-            text: "香蕉 1",
-            icon: "pen-to-square",
-            link: "1",
-          },
-          {
-            text: "香蕉 2",
-            icon: "pen-to-square",
-            link: "2",
-          },
-          "3",
-          "4",
+          "integer",
         ],
       },
-      { text: "樱桃", icon: "pen-to-square", link: "cherry" },
-      { text: "火龙果", icon: "pen-to-square", link: "dragonfruit" },
-      "tomato",
-      "strawberry",
     ],
   },
   {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
+    text: "MongoDB",
+    icon: "pen-to-square",
+    prefix: "/mongo/",
+    children: [
+      {
+        text: "Transaction",
+        icon: "pen-to-square",
+        link: "transaction"
+      },
+    ],
   },
 ]);
--- docs/.vuepress/sidebar.ts
+++ docs/.vuepress/sidebar.ts
@@ -3,20 +3,13 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     "",
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文章",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
     "intro",
-    "slides",
+    {
+      text: "MySQL",
+      icon: "book",
+      prefix: "mysql/",
+      children: ["innodb", "date_type"],
+    },
+    // "slides",
   ],
 });
